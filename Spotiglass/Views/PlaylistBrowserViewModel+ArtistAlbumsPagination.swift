59755385d3cbf8232e2318e20dc7f60e742d8a8f
TAALIIS4 <taaliis4@Isaacs-MacBook-Pro.fritz.box>
2026-05-08 20:04:23 +0200

    refactor(browser): carve PlaylistBrowserViewModel by concern
    - add PlaylistBrowserViewModel+ArtistAlbumsPagination.swift

---
+++ Spotiglass/Views/PlaylistBrowserViewModel+ArtistAlbumsPagination.swift
@@ -0,0 +1,67 @@
+import Foundation
+
+extension PlaylistBrowserViewModel {
+    func loadMoreArtistAlbums() async {
+        guard case let .loaded(.artist(detail)) = detailState,
+              var paging = currentArtistAlbumsPaging,
+              !paging.isLoading,
+              let nextURL = paging.nextURL else {
+            return
+        }
+
+        paging.isLoading = true
+        currentArtistAlbumsPaging = paging
+        detailState = .loaded(.artist(
+            ArtistDetailViewModel(
+                artist: detail.artist,
+                tracks: paging.tracks,
+                albums: paging.albums,
+                canLoadMoreAlbums: true,
+                isLoadingMoreAlbums: true
+            )
+        ))
+
+        do {
+            if paging.seenNextURLs.contains(nextURL.absoluteString) {
+                paging.nextURL = nil
+            } else {
+                paging.seenNextURLs.insert(nextURL.absoluteString)
+                let page = try await api.artistAlbumsPage(
+                    id: paging.artistID,
+                    includeGroups: paging.includeGroups,
+                    limit: paging.limit,
+                    offset: paging.nextOffset,
+                    nextURL: nextURL,
+                    cacheMode: .bypassCache
+                )
+                paging.albums = Self.dedupeAlbums(paging.albums + page.items)
+                paging.nextURL = page.next
+                paging.nextOffset += paging.limit
+            }
+            paging.isLoading = false
+            currentArtistAlbumsPaging = paging
+            let refreshed = ArtistDetailViewModel(
+                artist: detail.artist,
+                tracks: paging.tracks,
+                albums: paging.albums,
+                canLoadMoreAlbums: paging.nextURL != nil,
+                isLoadingMoreAlbums: false
+            )
+            cachedArtistSnapshots[detail.artist.id] = CachedArtistSnapshot(
+                snapshot: ArtistDetailSnapshot(
+                    artistDetail: detail.artist,
+                    albums: paging.albums,
+                    tracks: paging.tracks,
+                    usedStaleCache: false,
+                    paging: paging
+                ),
+                fetchedAt: now()
+            )
+            detailState = .loaded(.artist(refreshed))
+        } catch {
+            paging.isLoading = false
+            currentArtistAlbumsPaging = paging
+            detailState = .staleCache(.artist(detail), Self.displayError(for: error))
+        }
+    }
+}
