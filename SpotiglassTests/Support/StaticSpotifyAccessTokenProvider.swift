78e9a60fbe0a7a9bea8974f8198be142d7a19831
TAALIIS4 <taaliis4@Isaacs-MacBook-Pro.fritz.box>
2026-05-08 20:04:24 +0200

    test: add regression coverage for refactor
    - add StaticSpotifyAccessTokenProvider.swift

---
+++ SpotiglassTests/Support/StaticSpotifyAccessTokenProvider.swift
@@ -0,0 +1,14 @@
+import Foundation
+@testable import Spotiglass
+
+struct StaticSpotifyAccessTokenProvider: SpotifyAccessTokenProviding {
+    let token: String
+
+    func accessToken() async throws -> String {
+        token
+    }
+
+    func refreshAccessTokenAfterUnauthorized() async throws -> String {
+        token
+    }
+}
